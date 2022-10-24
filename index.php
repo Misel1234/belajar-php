4b7ed005ef530935c66bfb06f8d46dbe575b341f
Michelle Febi Syalom Hansang <michellehansang2gmail.com>
2022-10-24 14:12:24 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Michelle Febi Syalom Hansang";
+$umur = 20;
+
+echo "Nama saya <strong>$nama</strong>, saya $umur tahun.<br>";
+
+$namaKakak = "Eirene";
+$umurKakak = 32;
+
+echo "Nama Kakak saya <strong>$namaKakak</strong>, Kakak saya berusia $umurKakak tahun.<br>";
+
+
+$selisihUmur = $umur - $umurKakak;
+
+echo "Selisih usia saya dan Kakak saya adalah $selisihUmur tahun";
+
+?>
